7cf5557b631e7cd76474672cb36c2ed86b6178d1
Alumno <alumno@Gordon-4.local>
2017-11-04 20:41:34 -0600

    Add enviar.php and phpmailer plugin.

---
+++ forms/php/test.php
@@ -0,0 +1,15 @@
+<?php
+    $name=$_POST["formName"];
+    $tel=$_POST["formTel"];
+    $email=$_POST["formEmail"];
+    $url=$_POST["formUrl"];
+    $gender=$_POST["formGender"];
+    $hobbieRun=$_POST["formRun"];
+    $hobbieRead=$_POST["formRead"];
+    $hobieSwin=$_POST["formSwin"];
+    $nationality=$_POST["formNationality"];
+    $message=$_POST["formMessage"];
+
+    $customeMessage= "Hola, mi nombre es " . $name . " y mi teléfono es " . $tel . ".";
+
+?>
